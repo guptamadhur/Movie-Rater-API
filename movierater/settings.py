9542ad96556d1869806775264c67412cd225869f
gupta-madhur <madhur.gupta@go-mmt.com>
2020-12-26 21:05:59 +0530

    make it better

--- movierater/settings.py
+++ movierater/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY');
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG');
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = ['movie-rater-mg.herokuapp.com', '127.0.0.1', 'localhost', '192.168.29.10', ]
 
